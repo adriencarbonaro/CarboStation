4f04646c8f17036f62f1c52e530ed552cbe9ebd7
Adrien CARBONARO <adrien@orosound.com>
2020-01-26 23:13:28 +0100

    second commit, display first temp

--- app/src/main/java/com/carbostation/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/carbostation/ui/dashboard/DashboardFragment.java
@@ -1,6 +1,7 @@
 package com.carbostation.ui.dashboard;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,20 +24,52 @@ public class DashboardFragment extends Fragment {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        final TextView text_header = root.findViewById(R.id.text_dashboard_header);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
+        final TextView dashboard_title = root.findViewById(R.id.dashboard_title);
+        final TextView dashboard_temp_int_value = root.findViewById(R.id.dashboard_temp_int_value);
+        final TextView dashboard_temp_out_value = root.findViewById(R.id.dashboard_temp_out_value);
+        dashboardViewModel.getTitle().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                Log.d("A", s);
+                dashboard_title.setText(s);
             }
         });
-        dashboardViewModel.getText_header().observe(this, new Observer<String>() {
+        dashboardViewModel.getTempInt().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                text_header.setText(s);
+                Log.d("A", s);
+                dashboard_temp_int_value.setText(s);
+            }
+        });
+        dashboardViewModel.getTempOut().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                Log.d("A", s);
+                dashboard_temp_out_value.setText(s);
             }
         });
         return root;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        int cur_temp_int = Integer.parseInt(dashboardViewModel.getTextTempInt());
+        int cur_temp_out = Integer.parseInt(dashboardViewModel.getTextTempOut());
+        dashboardViewModel.setTextTempInt(String.valueOf(cur_temp_int+1));
+        dashboardViewModel.setTextTempOut(String.valueOf(cur_temp_out+1));
+        Log.d("M", String.valueOf(cur_temp_int));
+        Log.d("M", String.valueOf(cur_temp_out));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("M", "resume");
+        //View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        //TextView dashboard_title = root.findViewById(R.id.dashboard_title);
+        //TextView dashboard_temp_int_value = root.findViewById(R.id.dashboard_temp_int_value);
+        //TextView dashboard_temp_out_value = root.findViewById(R.id.dashboard_temp_out_value);
+        //dashboardViewModel.setText
     }
 }
--- app/src/main/java/com/carbostation/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/carbostation/ui/dashboard/DashboardViewModel.java
@@ -6,21 +6,49 @@ import androidx.lifecycle.ViewModel;
 
 public class DashboardViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
-    private MutableLiveData<String> mText_header;
+    private MutableLiveData<String> mDashboard_title;
+    private MutableLiveData<String> mDashboard_temp_int_value;
+    private MutableLiveData<String> mDashboard_temp_out_value;
 
     public DashboardViewModel() {
-        mText = new MutableLiveData<>();
-        mText_header = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
-        mText_header.setValue("Adrien");
+        mDashboard_title          = new MutableLiveData<>();
+        mDashboard_temp_int_value = new MutableLiveData<>();
+        mDashboard_temp_out_value = new MutableLiveData<>();
+        mDashboard_title.setValue("Temp.");
+        mDashboard_temp_int_value.setValue("23");
+        mDashboard_temp_out_value.setValue("09");
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    /* Live Data notifications */
+    public LiveData<String> getTitle() {
+        return mDashboard_title;
+    }
+    public LiveData<String> getTempInt() {
+        return mDashboard_temp_int_value;
+    }
+    public LiveData<String> getTempOut() {
+        return mDashboard_temp_out_value;
     }
 
-    public LiveData<String> getText_header() {
-        return mText_header;
+    /* String getters */
+    public String getTextTitle() {
+        return mDashboard_title.getValue();
+    }
+    public String getTextTempInt() {
+        return mDashboard_temp_int_value.getValue();
+    }
+    public String getTextTempOut() {
+        return mDashboard_temp_out_value.getValue();
+    }
+
+    /* String setters */
+    public void setTextTitle(String title) {
+        this.mDashboard_title.setValue(title);
+    }
+    public void setTextTempInt(String value) {
+        this.mDashboard_temp_int_value.setValue(value);
+    }
+    public void setTextTempOut(String value) {
+        this.mDashboard_temp_out_value.setValue(value);
     }
 }
