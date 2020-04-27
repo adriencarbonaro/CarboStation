084ecb978a5cbb04352a0096034e7741d4458cf3
Adrien CARBONARO <adrien@orosound.com>
2020-04-27 10:32:51 +0200

    use status fragment to display app version number

--- app/src/main/java/com/carbostation/ui/status/StatusFragment.java
+++ app/src/main/java/com/carbostation/ui/status/StatusFragment.java
@@ -10,13 +10,31 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.carbostation.R;
+import com.carbostation.BuildConfig;
 
 public class StatusFragment extends Fragment {
+
+    private TextView status_version = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.ui_fragment_status, container, false);
-        final TextView textView = root.findViewById(R.id.text_status);
+        status_version = root.findViewById(R.id.status_version);
         return root;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        updateView(BuildConfig.VERSION_NAME);
+    }
+
+    private void updateView(String version) {
+        status_version.setText(version);
     }
 }
