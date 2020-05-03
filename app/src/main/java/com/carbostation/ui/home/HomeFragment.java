5b8e12246a9e7aeb95b61822658ab267f00e60cb
Adrien CARBONARO <adrien@orosound.com>
2020-05-03 22:33:55 +0200

    update app name logo and colors

--- app/src/main/java/com/carbostation/ui/home/HomeFragment.java
+++ app/src/main/java/com/carbostation/ui/home/HomeFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,10 +15,12 @@ import com.carbostation.R;
 
 public class HomeFragment extends Fragment {
 
+    private ImageView home_logo     = null;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.ui_fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        home_logo = root.findViewById(R.id.home_logo);
         return root;
     }
 }
