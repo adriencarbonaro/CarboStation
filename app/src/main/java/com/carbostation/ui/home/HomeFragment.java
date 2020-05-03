024c77eb93a03212852afded6ac7d3a78616920c
Adrien CARBONARO <adrien@orosound.com>
2020-05-03 22:34:35 +0200

    add 2 test buttons

--- app/src/main/java/com/carbostation/ui/home/HomeFragment.java
+++ app/src/main/java/com/carbostation/ui/home/HomeFragment.java
@@ -16,11 +16,33 @@ import com.carbostation.R;
 public class HomeFragment extends Fragment {
 
     private ImageView home_logo     = null;
+    private Button button_cancel    = null;
+    private Button button_ok        = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.ui_fragment_home, container, false);
         home_logo = root.findViewById(R.id.home_logo);
+        button_cancel = root.findViewById(R.id.b1);
+        button_ok = root.findViewById(R.id.b2);
+        button_cancel.setOnClickListener(onClickHandler_cancer);
+        button_ok.setOnClickListener(onClickHandler_ok);
         return root;
     }
+
+    private Button.OnClickListener onClickHandler_cancer =
+        new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Cancel", Toast.LENGTH_LONG).show();
+            }
+        };
+
+    private Button.OnClickListener onClickHandler_ok =
+            new Button.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getContext(), "OK", Toast.LENGTH_LONG).show();
+                }
+            };
 }
